426d58f4986e45264d81851134c10e69a711173e
thyrons <willynarvaez6@gmail.com>
2015-05-26 11:46:08 -0500

    menu
    por fin menu

--- data/menu/menu.php
+++ data/menu/menu.php
@@ -61,46 +61,86 @@ function banner(){
 // menu principal lateral
 function menu_lateral(){
 print'
-<aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <div class="user-panel">            
-            <div class="pull-left info">
-              <p>SGD</p>              
-            </div>
-          </div>          
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header">Menú Principal</li>                                    
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-share"></i> <span>Multilevel</span>
-                <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-                <li>
-                  <a href="#"><i class="fa fa-circle-o"></i> Level One <i class="fa fa-angle-left pull-right"></i></a>
-                  <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-                    <li>
-                      <a href="#"><i class="fa fa-circle-o"></i> Level Two <i class="fa fa-angle-left pull-right"></i></a>
-                      <ul class="treeview-menu">
-                        <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                        <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-              </ul>
-            </li>            
-          </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">            
+        <div class="pull-left info">
+          <p>SGD</p>              
+        </div>
+      </div>          
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">Menú Principal</li>                                    
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-desktop"></i> <span>Generales</span>
+              <i class="fa fa-angle-left pull-right"></i>
+            </a>              
+            <ul class="treeview-menu">
+              <li><a href="../categorias"><i class="fa fa-plus-square-o"></i>Categorías</a></li>                
+              <li><a href="../departamentos"><i class="fa fa-sitemap"></i>Departamentos</a></li>
+              <li><a href="../medio_recepcion"><i class="fa fa-folder-open-o"></i>Medio Recepción</a></li>
+              <li><a href="../tipo_documento"><i class="fa fa-file-text-o"></i>Tipo Documento</a></li>
+              <li><a href="../tipo_usuario"><i class="fa fa-user"></i>Tipo Usuario</a></li>              
+              <li>
+                <a href="#"><i class="fa fa-flag-o"></i> Direcciónes <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="../pais"><i class="fa fa-flag-checkered"></i> Países</a></li>
+                  <li><a href="../provincia"><i class="fa fa-flag-checkered"></i> Provincias</a></li>
+                  <li><a href="../ciudad"><i class="fa fa-flag-checkered"></i> Ciudades</a></li>                  
+                </ul>
+              </li>
+            </ul>
+          </li>                    
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-files-o"></i> <span>Gestión Documental</span>
+              <i class="fa fa-angle-left pull-right"></i>
+            </a>              
+            <ul class="treeview-menu">
+              <li><a href="../envio"><i class="fa fa-envelope-o"></i> Redactar</a></li>                              
+              <li><a href="../inbox"><i class="fa fa-mail-forward"></i> Enviados</a></li>
+              <li><a href="#"><i class="fa fa-mail-reply"></i> Recibidos</a></li>
+            </ul>
+          </li>      
+           <li class="treeview">
+            <a href="#">
+              <i class="fa fa-dashboard"></i> <span>Reportes</span>
+              <i class="fa fa-angle-left pull-right"></i>
+            </a>              
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-search-plus"></i> Buscar Archivos</a></li>                                            
+              <li><a href="#"><i class="fa fa-bar-chart-o"></i> Estadisticas</a></li>
+              <li><a href="#"><i class="fa fa-book"></i> Reportes</a></li>                  
+            </ul>
+          </li>       
 
-	';
+          <li class="treeview">
+            <a href="#">
+              <i class="fa fa-gears"></i> <span>Administración</span>
+              <i class="fa fa-angle-left pull-right"></i>
+            </a>              
+            <ul class="treeview-menu">
+              <li><a href="#"><i class="fa fa-users"></i> Nuevos Usuarios</a></li>                              
+              <li><a href="#"><i class="fa fa-search-plus"></i> Buscar Archivos</a></li>                                            
+              <li><a href="#"><i class="fa fa-bullseye"></i> Restaurar Archivos</a></li>
+              <li><a href="#"><i class="fa fa-download"></i> Backup</a></li>
+              <li>
+                <a href="#"><i class="fa fa-dashboard"></i>Reportes Generales <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                  <li><a href="#"><i class="fa fa-bar-chart-o"></i> Estadisticas Generales</a></li>                  
+                  <li><a href="#"><i class="fa fa-book"></i> Reportes Generales</a></li>                  
+                </ul>
+              </li>
+                
+            </ul>
+          </li>  
+        </ul>
+      </section>
+    <!-- /.sidebar -->
+  </aside>
+';
 }
 ?>
