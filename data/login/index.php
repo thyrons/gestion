0b62122e682a67ae1b3b0b2a171f6017d4dedf07
Deivid <deividscriollo@gmail.com>
2015-06-04 16:56:08 -0500

    procesando

--- data/login/index.php
+++ data/login/index.php
@@ -12,6 +12,7 @@
     <link href="../../font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
     <link href="../../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../plugins/gritter-master/css/jquery.gritter.css" rel="stylesheet" type="text/css" />
     <!-- iCheck -->
     <link href="../../plugins/iCheck/square/blue.css" rel="stylesheet" type="text/css" />
 
@@ -61,6 +62,7 @@
     <script src="../../plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="../../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../../plugins/gritter-master/js/jquery.gritter.min.js" type="text/javascript"></script>
     <!-- iCheck -->
     <script src="../../plugins/iCheck/icheck.min.js" type="text/javascript"></script>  
     <script src="../../dist/js/bootbox.js"></script>  
@@ -68,7 +70,28 @@
     <script src="login.js" type="text/javascript"></script>    
     <script src="../funciones_generales.js"></script>    
     <script type="text/javascript">
-  // bootbox.alert("Hello world!");
+    // advertencia
+      $.gritter.add({
+        title: 'This is a regular notice!',
+        text: 'This will fade out after a certain amount of time.',
+        image: '../../dist/img/advertencia.fw.png',
+        class_name: 'dc_ok'
+      });
+      // error
+      $.gritter.add({
+        title: 'This is a regular notice!',
+        text: 'This will fade out after a certain amount of time.',
+        image: '../../dist/img/error.fw.png',
+        class_name: 'dc_ok'
+      });
+      // ok
+      $.gritter.add({
+        title: 'This is a regular notice!',
+        text: 'This will fade out after a certain amount of time.',
+        image: '../../dist/img/ok.fw.png',
+        sticky: false, 
+        class_name: 'dc_ok'
+      });
     </script>
   </body>
 </html>
